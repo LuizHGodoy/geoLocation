f45b1218ae93d4472d6c5ddaf48ea7b4f15a4ede
Luiz <luiz@pop-os.localdomain>
2022-06-23 11:02:48 -0400

    geolocation expo app

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import MapView, { Callout, Circle, Marker } from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import * as Location from "expo-location";
 
 export default function App() {
+  const [pin, setPin] = React.useState({
+    latitude: -22.22394425412641,
+    longitude: -54.80575952678919,
+  });
+
+  React.useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: -22.223768931995682,
+          longitude: -54.80467563504819,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+      >
+        <Marker
+          coordinate={pin}
+          pinColor="red"
+          draggable={true}
+          onDragStart={(e) => {
+            console.log("drag start", e.nativeEvent.coordinate);
+          }}
+          onDragEnd={(e) => {
+            console.log("drag end", e.nativeEvent.coordinate);
+
+            setPin({
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            });
+          }}
+        >
+          <Callout>
+            <Text>callout teste</Text>
+          </Callout>
+        </Marker>
+        <Circle center={pin} radius={100} />
+      </MapView>
     </View>
   );
 }
@@ -13,8 +62,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
